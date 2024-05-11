4e413eaccffafc254e0afaec51f91900e6d260b4
Reinaldo Augusto Bocchi <reinaldo.bocchi@gmail.com>
2024-05-11 11:23:56 -0300

    add infra and gha workflow

---
+++ provider.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
+
+# terraform {
+#   required_version = ">= 0.15"
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 3.0"
+#     }
+#   }
+# }
